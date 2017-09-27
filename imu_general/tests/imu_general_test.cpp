5d38a6e4e5e6f28ddcc1f2e119b5f1e8a5b4819d
mfonken <mgfonken@gmail.com>
2017-09-26 23:04:36 -0600

    Updated imu_general with separated test framework

--- imu_general/tests/imu_general_test.cpp
+++ imu_general/tests/imu_general_test.cpp
@@ -1,49 +1,40 @@
-//
-//  imu_general_test.cpp
-//  
-//
-//  Created by Matthew Fonken on 9/9/17.
-//
-//
-
 #include <stdio.h>
-#include "../utilities/i2c_sp.h"
-#include "../sensors/LSM9DS1.h"
-//#include "../kinetic/kinetic.h"
+#include <string>
+#include "../utilities/i2c_sp.c"
+#include "../sensors/LSM9DS1.c"
+#include "../kinetic/kinetic.c"
 
-int main( int argc, char * args[] )
+cartesian2_t bns[2];
+LSM9DS1_t imu;
+kinetic_t kin;
+
+void imu_general_setup( std::string msg )
 {
 
-   	printf("Starting...\n");
-	LSM9DS1_t lsm9;
-	//kinetic_t kinetics;
-	
-	printf("Initing I2C...\n");
-	I2C_Init();
-    printf("Initing IMU...\n");
-    IMU_Init(&lsm9);
+    printf("Starting...\n");
+    printf("Initing I2C...\n");
+    I2C_Init();
     
-/*
-
-	printf("Initing kinetics...\n");
-
-    	Kinetic_Init(&lsm9, &kinetics);
-
-
-	//printf("R:%.2x > V:%.2x > A:%.2x\n", WHO_AM_I, IMU_WHO_AM_I_VAL, IMU_GetRegister(WHO_AM_I));
-	//printf("R:%.2x > V:%.2x > A:%.2x\n", WHO_AM_I_M, MAG_WHO_AM_I_VAL, Mag_GetRegister(WHO_AM_I_M));
-
-	if(IMU_Verify(&lsm9)) printf("IMU verification successful.\n");
-	else printf("IMU verification failed!\n");
+    printf("Initing beacons.\n");
     
-    	printf("Updating IMU...\n");
-    	IMU_Update_All(&lsm9);
+    bns[0].x = 400;
+    bns[0].y = 100;
+    bns[1].x = 100;
+    bns[1].y = 400;
     
-	for(int i = 0; i < 1000; i++)
-	{
-		Kinetic_Update_Rotation(&lsm9, &kinetics);
-		printf("A<%.2f. %.2f,%.2f>\r\n", kinetics.rotation[0], kinetics.rotation[1], kinetics.rotation[2]);
-	}
-*/
-    	return 0;
+    printf("Initing IMU.\n");
+    IMU_Init(&imu);
+    
+    printf("Initing kinetic.\n");
+    Kinetic_Init( &imu, &kin );
 }
+
+void imu_general_main( std::string msg )
+{
+    printf("Updating position.\n");
+    Kinetic_Update_Position( &imu, &kin, bns );
+    char buff[100];
+    sprintf(buff, "P:(%.2f, %.2f, %.2f)\n", kin.truePosition[0], kin.truePosition[1], kin.truePosition[2] );
+    msg = buff;
+}
+
