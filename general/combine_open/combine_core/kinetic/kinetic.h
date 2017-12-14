b6eb7b60c18762c83fb83825451850cb180ce5e9
mfonken <mgfonken@gmail.com>
2017-12-14 00:06:59 -0700

    Unit update to kinetic

--- general/combine_open/combine_core/kinetic/kinetic.h
+++ general/combine_open/combine_core/kinetic/kinetic.h
@@ -1,11 +1,11 @@
 /*! \file spatial.h
     \brief Spatial Orientation Main\r\n
- 
+
  NOTES:
  - IMU is for rotational data ONLY!
  - Positional data comes from camera triangulation module and beacons
  - Right-handed rotation
- 
+
   Created by Matthew Fonken on 10/8/16.
  */
 
@@ -26,7 +26,7 @@ extern "C" {
 
 /* Types */
 #include "kinetic_types.h"
-    
+
 /* Utilities */
 #include "../open/global_types.h"
 
@@ -51,57 +51,57 @@ extern "C" {
 /***************************************************************************************************
  Global Definitions
 ***************************************************************************************************/
-    
+
 /** Absolute value */
 #define 	absl(x) x > 0 ? x:-x
-    
+
 /** Initial normal unit vector to beacon plane */
 #define     VISION_ZSTATE_I  0
 #define     VISION_ZSTATE_J  0
 #define     VISION_ZSTATE_K -1
-    
+
     /* Camera Precalculated values */
 #define     CAMERA_OFFSET_ANGLE_X   0       // degrees
 #define     CAMERA_OFFSET_ANGLE_Y   0       // degress
 #define     CAMERA_OFFSET_ANGLE_Z   0       // degrees
-    
+
 //#define     CAMERA_ALPHA_W          120     // degrees
 //#define     CAMERA_ALPHA_H          75      // degrees
-    
+
 #define     CAMERA_WIDTH            640//1280    // pixels
 #define     CAMERA_HEIGHT           400//800     // pixels
 #define     CAMERA_HALF_WIDTH       CAMERA_WIDTH/2
 #define     CAMERA_HALF_HEIGHT      CAMERA_HEIGHT/2
-    
-    
-#define     UNITS                  "cm"
+
+
+#define     UNITS                  "m"//"cm"
 #define     PIXEL_TO_UNIT           3e-6
 #define     UNIT_TO_PIXEL         ( 1.0 / PIXEL_TO_UNIT )
-    
+
 #define     FOCAL_REFRACTION        1.42e-6//1.42e-6//  // units / units
-#define     FOCAL_LENGTH          ( 3.5e-3 * FOCAL_REFRACTION ) * UNIT_TO_PIXEL // dimension units
-    
+#define     FOCAL_LENGTH            3.5e-3 * FOCAL_REFRACTION // units
+
 #define     D_FIXED                 5.08e-2//3.4e-2
-    
+
 /***************************************************************************************************
 Function Declarations
 ***************************************************************************************************/
-    
+
 /***********************************************************************************************//**
  *  \brief  Initialize Kinetic Sensors
  **************************************************************************************************/
 void Kinetic_Init( LSM9DS1_t *, kinetic_t * );
-    
+
 /***********************************************************************************************//**
  *  \brief  Initialize Filters for Kinetic Data
  **************************************************************************************************/
 void Filters_Init( LSM9DS1_t * imu, kinetic_t * kinetics );
-    
+
 /***********************************************************************************************//**
  *  \brief  Update rotation filter data
  **************************************************************************************************/
 void Kinetic_Update_Rotation( LSM9DS1_t * imu, kinetic_t * kinetics );
-    
+
 /***********************************************************************************************//**
  *  \brief  Update position filter data
  **************************************************************************************************/
@@ -110,7 +110,7 @@ void Camera_Rotation_Init( void );
 
 /** @} (end addtogroup kinetic) */
 /** @} (end addtogroup Application) */
-    
+
 #ifdef __cplusplus
 };
 #endif
