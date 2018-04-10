382b23f81e051ef4b9f92d521f220e78ec8ec04b
mfonken <mgfonken@gmail.com>
2018-04-10 16:05:45 -0600

    Added combine_rtos_10

---
+++ master/CRTOS/combine_rtos_10/combine_rtos_10/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  combine_rtos_10
+//
+//  Created by Matthew Fonken on 4/10/18.
+//  Copyright © 2018 Marbl. All rights reserved.
+//
+
+#include <iostream>
+
+#include "rtos_main.c"
+
+int main(int argc, const char * argv[]) {
+    rtos_main();
+    return 0; 
+}
