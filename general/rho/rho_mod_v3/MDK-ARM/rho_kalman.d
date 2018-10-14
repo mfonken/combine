7a624f6654e792c0b012d1769f00ba039386bfd2
mfonken <mgfonken@gmail.com>
2018-10-14 16:36:10 -0600

    rho_mod update

--- general/rho/rho_mod_v3/MDK-ARM/rho_kalman.d
+++ general/rho/rho_mod_v3/MDK-ARM/rho_kalman.d
@@ -1,34 +1,37 @@
-./rho_kalman.o: ../Src/rho/rho_kalman.c ../Src/rho/rho_kalman.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc\stm32l4xx_hal.h \
-  ../Inc\stm32l4xx_hal_conf.h ../Inc/main.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc\stm32l4xx_hal_rcc.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h \
-  ../Drivers/CMSIS/Device/ST/STM32L4xx/Include\stm32l4xx.h \
-  ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l432xx.h \
-  ../Drivers/CMSIS/Include\core_cm4.h \
-  ../Drivers/CMSIS/Include/core_cmInstr.h \
-  ../Drivers/CMSIS/Include/cmsis_armcc_V6.h \
-  ../Drivers/CMSIS/Include/core_cmFunc.h \
-  ../Drivers/CMSIS/Include/core_cmSimd.h \
-  ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc\stm32l4xx_hal_gpio.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc\stm32l4xx_hal_dma.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc\stm32l4xx_hal_dma_ex.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc\stm32l4xx_hal_cortex.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc\stm32l4xx_hal_flash.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc\stm32l4xx_hal_i2c.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc\stm32l4xx_hal_pwr.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc\stm32l4xx_hal_tim.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc\stm32l4xx_hal_uart.h \
-  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h
+.\rho_kalman.o: ..\Src\rho\rho_kalman.c
+.\rho_kalman.o: ..\Src\rho\rho_kalman.h
+.\rho_kalman.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\rho_kalman.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\rho_kalman.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h
+.\rho_kalman.o: ../Inc/stm32l4xx_hal_conf.h
+.\rho_kalman.o: ../Inc/main.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h
+.\rho_kalman.o: ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h
+.\rho_kalman.o: ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l432xx.h
+.\rho_kalman.o: ../Drivers/CMSIS/Include/core_cm4.h
+.\rho_kalman.o: ../Drivers/CMSIS/Include/core_cmInstr.h
+.\rho_kalman.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+.\rho_kalman.o: ../Drivers/CMSIS/Include/core_cmFunc.h
+.\rho_kalman.o: ../Drivers/CMSIS/Include/core_cmSimd.h
+.\rho_kalman.o: ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h
+.\rho_kalman.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h
